6e34839f4ddf986c03df3fa07f60d73832425603
Iver Dihle Skjervum <iver.ds@gmail.com>
2012-09-19 10:40:59 +0200

    Removed testcode

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ApplicationController
   def index
-    respond_to do |format|
-      format.json {render json: ["Verryo", "Funnyyy"]}
-      format.html  #index.html.erb
-    end
+
   end
 end
