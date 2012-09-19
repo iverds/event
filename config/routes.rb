bb83fd1d9e2e2897871b76626be95a944b119c4e
Iver Dihle Skjervum <iver.ds@gmail.com>
2012-09-19 13:55:31 +0200

    Lagt til article - struktur

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Event::Application.routes.draw do
   get "home/index"
-
+  resources :articles
   resources :isfit_events do
     get :on_date, on: :collection
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120918123706) do
+ActiveRecord::Schema.define(:version => 20120919113731) do
+
+  create_table "articles", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "isfit_events", :force => true do |t|
     t.datetime "start_date"
